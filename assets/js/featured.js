7ee611085b006cb3d1ecfc8b1d94af8aa286c8d5
DESKTOP-E4UEJB8\DELL <tazwar70@gmail.com>
2021-03-12 22:31:59 -0500

    overhaul update

---
+++ assets/js/featured.js
@@ -0,0 +1,87 @@
+document.getElementById('testimonials').innerHTML=
+`
+<div class="container" data-aos="fade-up">
+
+      <div class="section-title">
+        <h2>Featured Items</h2>
+        <p>These are some of my highlights.</p>
+      </div>
+
+      <div class="owl-carousel testimonials-carousel">
+
+        <div class="testimonial-item">
+          <img src="assets/Featured/ICREST - 20114 - Md Shadman Tajwar Haque.jpg" class="testimonial-img" alt="">
+          <h3><a href="https://ieeexplore.ieee.org/document/8644212">Design and Implementation of an IoT based Automated Agricultural Monitoring and Control System</a></h3>
+          <h4>Publication</h4>
+          <p>
+            <!-- <i class="bx bxs-quote-alt-left quote-icon-left"></i> -->
+            Published at: 2019 International Conference on Robotics,Electrical and Signal Processing Techniques (ICREST)
+            <!-- <i class="bx bxs-quote-alt-right quote-icon-right"></i> -->
+          </p>
+        </div>
+
+        <div class="testimonial-item">
+          <img src="/assets/Featured/Cognitive Class - Data Analysis with Python.jpg" class="testimonial-img" alt="">
+          <h3>Data Analysis with Python</h3>
+          <h4>Online Course<br>by<br></h4>
+          <p>
+            <!-- <i class="bx bxs-quote-alt-left quote-icon-left"></i> -->
+            Cognitive Class
+            <!-- <i class="bx bxs-quote-alt-right quote-icon-right"></i> -->
+          </p>
+        </div>
+
+        <div class="testimonial-item">
+          <img src="/assets/Featured/Cognitive Class - Machine Learning with Python.jpg" class="testimonial-img" alt="">
+          <h3>Machine Learning with Python</h3>
+          <h4>Online Course<br>by<br></h4>
+          <p>
+            <!-- <i class="bx bxs-quote-alt-left quote-icon-left"></i> -->
+            Cognitive Class
+            <!-- <i class="bx bxs-quote-alt-right quote-icon-right"></i> -->
+          </p>
+        </div>
+
+        <div class="testimonial-item">
+          <img src="/assets/Featured/Cognitive Class - Python 101 for Data Science.jpg" class="testimonial-img" alt="">
+          <h3>Python 101 for Data Science</h3>
+          <h4>Online Course<br>by<br></h4>
+          <p>
+            <!-- <i class="bx bxs-quote-alt-left quote-icon-left"></i> -->
+            Cognitive Class
+            <!-- <i class="bx bxs-quote-alt-right quote-icon-right"></i> -->
+          </p>
+        </div>
+
+        <div class="testimonial-item">
+          <img src="/assets/Featured/Coursera - Data Science Math - Duke University-1.jpg" class="testimonial-img" alt="">
+          <h3>Data Science Math Skills</h3>
+          <h4>Online Course<br>by<br></h4>
+          <p>
+            <!-- <i class="bx bxs-quote-alt-left quote-icon-left"></i> -->
+            Duke University, Coursera
+            <!-- <i class="bx bxs-quote-alt-right quote-icon-right"></i> -->
+          </p>
+        </div>
+
+        <div class="testimonial-item">
+          <img src="/assets/Featured/Coursera - Computer Vision Basics - UofBuffalo-1.jpg" class="testimonial-img" alt="">
+          <h3>Computer Vision Basics</h3>
+          <h4>Online Course<br>by<br></h4>
+          <p>
+            <!-- <i class="bx bxs-quote-alt-left quote-icon-left"></i> -->
+            University of Buffalo, Coursera
+            <!-- <i class="bx bxs-quote-alt-right quote-icon-right"></i> -->
+          </p>
+        </div>
+
+       
+
+      </div>
+
+      <div id="btn-ctn" class="text-center" style="padding-bottom: 1rem;">
+      <a href="assets/Md Shadman Tajwar Haque - Resume.pdf" class="btn-about btn-lg" style="font-size:1rem;">View Resume</a>
+    </div>
+
+    </div>
+`
