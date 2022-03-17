bf0e4ef74cf5d086b3d29b1e9f8a5a01d12cbde4
DESKTOP-E4UEJB8\DELL <tazwar70@gmail.com>
2022-03-17 17:09:00 -0400

    Update footer.js

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -4,8 +4,6 @@ document.getElementById("footer").innerHTML =
       <ul class="list-inline">
         <li class="list-inline-item"><a href="https://twitter.com/TazwarH"><i class="fab fa-twitter"></i></a></li>
         <li class="list-inline-item"><a href="https://github.com/tazwar70?tab=repositories"><i class="fab fa-github"></i></a></li>
-        <li class="list-inline-item"><a href="https://www.instagram.com/tajwarscribbles"><i class="fab fa-instagram"></i></a></li>
-        <li class="list-inline-item"><a href="https://www.deviantart.com/tajwarscribbles"><i class="fab fa-deviantart"></i></a></li>
         <li class="list-inline-item"><a href="https://www.instagram.com/tazwar70"><i class="fab fa-instagram"></i></a></li>
     </ul>
     <ul class="copyright"><li>2020 &copy; Md Shadman Tajwar Haque</li>
